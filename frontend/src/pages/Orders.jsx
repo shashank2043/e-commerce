d5c61acca5d63a11df87c1afa197161dae58f1df
PULLURI SHASHANK <shashankpulluri1@gmail.com>
2025-05-23 15:58:16 +0530

    Created Login and Sign Up pages

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -32,7 +32,7 @@ const Orders = () => {
                   <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
                   <p className='text-sm md:text-base'>Ready to ship</p>
                 </div>
-                <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
+                <button className='border px-4 py-2 text-sm font-medium rounded-sm cursor-pointer'>Track Order</button>
               </div>
             </div>
           ))
