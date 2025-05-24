1394461dadc9de2fa38001d7b6e0c25714a4189b
PULLURI SHASHANK <shashankpulluri1@gmail.com>
2025-05-24 14:15:29 +0530

    Implemented add product logic.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,32 @@
-import validator from 'validator';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken'
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
-const createToken = (id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET)
-}
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 // Route for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Route for user registration
 const registerUser = async (req, res) => {
@@ -21,29 +39,34 @@ const registerUser = async (req, res) => {
     }
     // validating email format & strong password
     if (!validator.isEmail(email)) {
-        return res.json({ success: false, message: "Please enter a valid email" });
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
     }
     if (password.length < 8) {
-        return res.json({ success: false, message: "Please enter a strong password" });
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
     }
     // hashing user password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new userModel({
-        name,
-        email,
-        password:hashedPassword,
-    })
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-    const user = await newUser.save()
+    const user = await newUser.save();
 
-    const token = createToken(user._id)
+    const token = createToken(user._id);
 
-    res.json({success:true,token})
-
+    res.json({ success: true, token });
   } catch (error) {
     console.log(error);
-    res.json({success:false, message:error.message})
+    res.json({ success: false, message: error.message });
   }
 };
 // Route for admin login
