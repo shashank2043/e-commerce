faf46092a1452c5a641d71e6a2f05f3811ab2b8a
PULLURI SHASHANK <shashankpulluri1@gmail.com>
2025-05-31 01:31:45 +0530

    Created admin panel folder structure

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,6 +70,27 @@ const registerUser = async (req, res) => {
   }
 };
 // Route for admin login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email);
+    console.log(password);
+    console.log(process.env.ADMIN_EMAIL);
+    console.log(process.env.ADMIN_PASSWORD);
+    
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Inavid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { loginUser, registerUser, adminLogin };
